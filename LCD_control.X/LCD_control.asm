92cf6a135b61a65e58165e881f56da92d976b939
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-10-30 00:42:32 -0300

    Add routines for init and complete 4 bit command
    TODO:
    
        Extensive tests

--- LCD_control.X/LCD_control.asm
+++ LCD_control.X/LCD_control.asm
@@ -11,10 +11,55 @@
 .equ LIGAR_LCD = 0b0000_1111
 .equ CONFIG_LCD = 0b0010_0100
 
+init:
+    ldi r16, 0xFF
+    out DDRD, r16
+    rcall LCD_init
+nops:
+    nop
+rjmp nops
+
+LCD_init:
+    ldi r16, 0b0011_0000
+    rcall LCD_H
+    rcall delay_5ms
+    rcall LCD_H
+    rcall delay_500us
+    rcall LCD_H
+    rcall delay_45us
+    ldi r16, CONFIG_LCD
+    rcall LCD_H
+    rcall delay_45us
+    rcall LCD_command
+    rcall delay_45us
+    ldi r16, DESLIGAR_LCD
+    rcall LCD_command
+    rcall delay_45us
+    ldi r16, LIMPAR_LCD
+    rcall delay_2ms
+    ldi r16, MODO_ENTRADA
+    rcall LCD_command
+    rcall delay_45us
+ret
+
+; r16 loaded with command
+; TODO: check if flags E and R are correct
+LCD_command:
+    push r20
+    mov r20, r16
+    andi r16, 0b1111_0000
+    rcall LCD_H
+    mov r16, r20
+    lsl r16
+    lsl r16
+    lsl r16
+    lsl r16
+    rcall LCD_L
+ret
 
 ;r16 deve possuír os 4 bits a serem anviados em 0b1111_0000
 LCD_L:
-    out r16, PORTD
+    out PORTD, r16
     call delay_45us
     sbi PORTD, E
     call delay_45us
@@ -24,7 +69,7 @@ LCD_L:
 
 ;r16 deve possuír os 4 bits a serem anviados em 0b1111_0000
 LCD_H:
-    out r16, PORTD
+    out PORTD, r16 
     call delay_45us
     sbi PORTD, E
     call delay_45us
@@ -33,24 +78,40 @@ LCD_H:
     ret
 
 delay_45us:
+    push r20
     ldi r20, 240
     loop0:
         dec r20
         brne loop0
+    pop r20
     ret
 
 delay_500us:
-    ldi r21, 12
+    push r20
+    ldi r20, 12
     loop1:
-        dec r21
-        call delay_50us
+        dec r20
+        call delay_45us
         brne loop1
+    pop r20
     ret
 
 delay_2ms:
-    ldi r22, 45
+    push r20
+    ldi r20, 45
     loop2:
-        dec r22
-        call delay_50us
+        dec r20
+        call delay_45us
         brne loop2
+    pop r20
+    ret
+
+delay_5ms:
+    push r20
+    ldi r20, 110
+    loop3:
+        dec r20
+        call delay_45us
+        brne loop3
+    pop r20
     ret
