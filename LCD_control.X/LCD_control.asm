47d1ad454cd7429d63b2f678f9857353ef5f6047
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-11-02 16:44:30 -0300

    Blinks LCD coursor (tested)

--- LCD_control.X/LCD_control.asm
+++ LCD_control.X/LCD_control.asm
@@ -1,80 +1,101 @@
-.cseg
-.org 0x34
-
 .equ RS = 2
 .equ E = 3
 .equ PD = PORTD
 
 .equ LIMPAR_LCD = 0b0000_0001
 .equ MODO_ENTRADA = 0b0000_0111
-.equ DESLIGAR_LCD = 0b0000_1000
+.equ DESLIGAR_LCD =  0b0000_1000
 .equ LIGAR_LCD = 0b0000_1111
-.equ CONFIG_LCD = 0b0010_0100
+.equ CONFIG_LCD = 0b0010_0000
+
+.cseg
+.org 0x00
 
 init:
+    ldi r20, high(RAMEND)
+    sts SPH, r20
+    ldi r20, low(RAMEND)
+    sts SPL, r20
+
     ldi r16, 0xFF
     out DDRD, r16
-    rcall LCD_init
+
 nops:
+    rcall LCD_init
     nop
-rjmp nops
+    rcall delay_1s
+    ldi r16, LIGAR_LCD
+    rcall LCD_command
+    rcall delay_1s
+    ldi r16, 0b0000_0010
+    rcall LCD_command
+    rcall delay_1s
+    
+    rjmp nops
+
 
 LCD_init:
+    rcall delay_45ms
     ldi r16, 0b0011_0000
-    rcall LCD_H
+    rcall LCD_4bits
+
     rcall delay_5ms
-    rcall LCD_H
+
+    ldi r16, 0b0011_0000
+    rcall LCD_4bits
     rcall delay_500us
-    rcall LCD_H
+
+    ldi r16, 0b0011_0000
+    rcall LCD_4bits
     rcall delay_45us
+
     ldi r16, CONFIG_LCD
-    rcall LCD_H
+    andi r16, 0b1111_0000
+    rcall LCD_4bits
     rcall delay_45us
+
+    ldi r16, CONFIG_LCD
     rcall LCD_command
     rcall delay_45us
+
     ldi r16, DESLIGAR_LCD
     rcall LCD_command
     rcall delay_45us
+
     ldi r16, LIMPAR_LCD
     rcall delay_2ms
+
     ldi r16, MODO_ENTRADA
     rcall LCD_command
     rcall delay_45us
 ret
-
 ; r16 loaded with command
 ; TODO: check if flags E and R are correct
 LCD_command:
     push r20
     mov r20, r16
     andi r16, 0b1111_0000
-    rcall LCD_H
+    rcall LCD_4bits
     mov r16, r20
     lsl r16
     lsl r16
     lsl r16
     lsl r16
-    rcall LCD_L
+    andi r16, 0b1111_0000
+    rcall LCD_4bits
+    pop r20
 ret
 
 ;r16 deve possuír os 4 bits a serem anviados em 0b1111_0000
-LCD_L:
-    out PORTD, r16
-    call delay_45us
-    sbi PORTD, E
-    call delay_45us
-    cbi PORTD, E
-    call delay_45us
-    ret
-
-;r16 deve possuír os 4 bits a serem anviados em 0b1111_0000
-LCD_H:
+LCD_4bits:
     out PORTD, r16 
     call delay_45us
     sbi PORTD, E
     call delay_45us
     cbi PORTD, E
     call delay_45us
+    clr r16
+    out PORTD, r16
     ret
 
 delay_45us:
@@ -88,10 +109,10 @@ delay_45us:
 
 delay_500us:
     push r20
-    ldi r20, 12
+    ldi r20, 11
     loop1:
-        dec r20
         call delay_45us
+        dec r20
         brne loop1
     pop r20
     ret
@@ -100,8 +121,8 @@ delay_2ms:
     push r20
     ldi r20, 45
     loop2:
-        dec r20
         call delay_45us
+        dec r20
         brne loop2
     pop r20
     ret
@@ -110,8 +131,28 @@ delay_5ms:
     push r20
     ldi r20, 110
     loop3:
-        dec r20
         call delay_45us
+        dec r20
         brne loop3
+    pop r20
+    ret
+
+delay_45ms:
+    push r20
+    ldi r20, 9
+    loop4:
+        call delay_5ms
+        dec r20
+        brne loop4
+    pop r20
+    ret
+
+delay_1s:
+    push r20
+    ldi r20, 200
+    loop5:
+        call delay_5ms
+        dec r20
+        brne loop5
     pop r20
     ret
